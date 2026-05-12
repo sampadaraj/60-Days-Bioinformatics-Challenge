8d11775a2698a27104eba66d2aef5d46bae01b6a
sampadaraj <sampadaraj128@gmail.com>
2026-05-13 00:29:00 +0530

    Create Day1_Transcription.py

---
+++ Day1_Transcription.py
@@ -0,0 +1,8 @@
+#DAY 1 : DNA TO RNA Transcription
+#Biological logic: Transcription is the process where we convert RNA to DNA . The Thymine (T) replaces to Uracil (U)
+
+dna = "GATTAACTTTTTAAAAAACCCCCTTTTT"
+rna = dna.replace("T" , "U" )
+
+print("DNA Sequence:", dna)
+print("RNA Sequence:", rna)
